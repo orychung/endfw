69728ce580fd7b9bb116f3d3654ed2b13cfd2e0b
COY <ysmood@gmail.com>
2023-05-06 14:19:14 +0900

    fixing dependencies

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ if (typeof module === 'undefined') {
   // skip if not running node
 } else {
   module.exports = {
-    dynamic: require('server/dynamic'),
-    lessCss: require('server/lessCss'),
-    mssql: require('server/mssql'),
-    pg: require('server/pg'),
-    server: require('server/server'),
-    serverUtil: require('server/serverUtil'),
-    subroute: require('server/subroute'),
+    dynamic: require('./server/dynamic'),
+    lessCss: require('./server/lessCss'),
+    mssql: require('./server/mssql'),
+    pg: require('./server/pg'),
+    server: require('./server/server'),
+    serverUtil: require('./server/serverUtil'),
+    subroute: require('./server/subroute'),
     
-    builtin: require('common/builtin'),
-    dbBuiltin: require('common/dbBuiltin'),
-    global: require('common/global'),
-    parse: require('common/parse'),
-    performance: require('common/performance'),
-    promise: require('common/promise'),
-    stat: require('common/stat'),
-    text: require('common/text'),
+    builtin: require('./common/builtin'),
+    dbBuiltin: require('./common/dbBuiltin'),
+    global: require('./common/global'),
+    parse: require('./common/parse'),
+    performance: require('./common/performance'),
+    promise: require('./common/promise'),
+    stat: require('./common/stat'),
+    text: require('./common/text'),
     
-    matrix: require('math/matrix'),
+    matrix: require('./math/matrix'),
   }
 }
