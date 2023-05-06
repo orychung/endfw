afa41473ee8f0b5d7df3007391843f3efb43cd93
COY <ysmood@gmail.com>
2023-05-06 13:56:12 +0900

    add files for npm install
    * trying to make package.json for server use

---
+++ index.js
@@ -0,0 +1,25 @@
+// for being imported as node module
+if (typeof module === 'undefined') {
+  // skip if not running node
+} else {
+  module.exports = {
+    dynamic: require('server/dynamic'),
+    lessCss: require('server/lessCss'),
+    mssql: require('server/mssql'),
+    pg: require('server/pg'),
+    server: require('server/server'),
+    serverUtil: require('server/serverUtil'),
+    subroute: require('server/subroute'),
+    
+    builtin: require('common/builtin'),
+    dbBuiltin: require('common/dbBuiltin'),
+    global: require('common/global'),
+    parse: require('common/parse'),
+    performance: require('common/performance'),
+    promise: require('common/promise'),
+    stat: require('common/stat'),
+    text: require('common/text'),
+    
+    matrix: require('math/matrix'),
+  }
+}
