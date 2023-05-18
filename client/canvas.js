3287410d54b9a7d7ca20422cc39aae9b13b68eb2
COY <ysmood@gmail.com>
2023-05-18 22:49:17 +0900

    adding clearImage

--- client/canvas.js
+++ client/canvas.js
@@ -1,7 +1,18 @@
 "use strict";
+function addMethod(f) {
+    return {
+        enumerable: false,
+        writable: true,
+        value: f
+    };
+}
+
 Object.defineProperty(HTMLCanvasElement.prototype, 'ctx', {
   get() {
     if (!this._ctx) this._ctx = this.getContext('2d');
     return this._ctx;
   }
 });
+Object.defineProperty(HTMLCanvasElement.prototype, 'clearImage', addMethod(function clearImage() {
+  this.ctx.clearRect(0, 0, this.width, this.height);
+}));
